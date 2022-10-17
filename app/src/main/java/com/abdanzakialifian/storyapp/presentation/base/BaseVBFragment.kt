361a7d36115931cd1d36a6b98e8cff99b59e197d
abdanzakialifian <abdanzakialifian99@gmail.com>
2022-10-17 21:47:16 +0700

    First commit StoryApp

---
+++ app/src/main/java/com/abdanzakialifian/storyapp/presentation/base/BaseVBFragment.kt
@@ -0,0 +1,37 @@
+package com.abdanzakialifian.storyapp.presentation.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseVBFragment<VB : ViewBinding> : Fragment() {
+    private var _binding: VB? = null
+    val binding get() = _binding as VB
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = getViewBinding()
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initView()
+    }
+
+    abstract fun getViewBinding(): VB
+
+    abstract fun initView()
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/abdanzakialifian/storyapp/presentation/container/ContainerActivity.kt
@@ -0,0 +1,12 @@
+package com.abdanzakialifian.storyapp.presentation.container
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.abdanzakialifian.storyapp.R
+
+class ContainerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_container)
+    }
+}
